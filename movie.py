6c1d192d36cfe126d9d26aec96e4ccb5f0ad70a4
whiteBerryJ <wjdwlsrbs77@naver.com>
2020-08-04 12:33:36 +0900

    movie_code_and_csv

--- movie.py
+++ movie.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 url = 'https://movie.naver.com/movie/running/current.nhn'
 req = requests.get(url)
@@ -9,7 +10,6 @@ movie_list = soup.select(
     'div[id=content] > div.article > div.obj_section > div.lst_wrap > ul > li')
 
 movie_code_list = []
-
 
 for movie in movie_list:
     movie_dict={}
@@ -22,5 +22,10 @@ for movie in movie_list:
         'code' : code
     }
     movie_code_list.append(movie_dict)
+    
+with open('movie_code.csv', 'w', newline='') as csvfile:
+    fieldnames = ['title','code']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writerows(movie_code_list)
 print(movie_code_list)
 
